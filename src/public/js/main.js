8a54bcbba7926c44720f02d4161dca574f8eb64e
estebanenve <esteban@enve.io>
2021-01-20 09:47:19 -0300

    geo

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,9 +1,11 @@
+//Ubicacion inicial del  mapa
 const map =L.map('map-template').setView([-34.6083,-58.3712],13);
 
+//Template a utilizar
 const tileURL= 'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png';
-
 L.tileLayer(tileURL).addTo(map);
 
+//Ubucacion actual
 map.locate({enableHighAccuracy:true});
 map.on('locationfound',e=>{
     const coords=[e.latlng.lat, e.latlng.lng];
@@ -11,17 +13,38 @@ map.on('locationfound',e=>{
     marker.bindPopup('Ubicacion actual');
     map.addLayer(marker);
 });
+
+//Marcar con doble click
 map.on('dblclick', e =>{
     let latLng = map.mouseEventToLatLng(e.originalEvent);
     const coords=[latLng.lat, latLng.lng];
-    console.log(latLng.lat)
     const marker=L.marker(coords);
     map.addLayer(marker);
 })
 
-map.doubleClickZoom.disable()
+//Cancelar zoom al hacer doble click
+map.doubleClickZoom.disable();
 
+//Crear un Popup en ubicacion en particular
 const marker=L.marker([-34.6083,-58.3712]);
 marker.bindPopup('Ciudad de buenos aires');
-
 map.addLayer(marker);
+
+//Geolocalizacion
+let markerGeo=null;
+const updateMap=()=>{
+    const urlGeo = 'http://api.open-notify.org/iss-now.json';
+fetch(urlGeo)
+.then(res=>res.json())
+.then(data=>{
+    if(markerGeo){
+        map.removeLayer(markerGeo)
+    }
+    const {latitude, longitude}=data.iss_position;
+    const coords=[latitude, longitude];
+    markerGeo=L.marker(coords).addTo(map)
+   
+})
+setTimeout(updateMap, 1000)
+}
+ updateMap()
